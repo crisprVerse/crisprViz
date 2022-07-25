5a593995b24c78a50e7432bf9e99fe277a11a613
Luke Hoberecht <hoberecl@gene.com>
2022-07-25 09:41:30 -0700

    Reorganized data documentation

--- R/data.R
+++ R/data.R
@@ -21,3 +21,102 @@
 #'     
 #' @usage data(Ideogram_GRCm38, package="crisprViz")
 "Ideogram_GRCm38"
+
+
+
+#' Example GuideSet targeting the human KRAS gene.
+#'
+#' An example \linkS4class{GuideSet} object targeting the CDS of the human KRAS
+#'     gene. Generated using the SpCas9 CRISPR nuclease, the genome sequence
+#'     from the \code{BSgenome.Hsapiens.UCSC.hg38} package, and the gene model
+#'     from \code{txdb_human} in the \code{crisprDesignData} package (Ensembl
+#'     release 104).
+#' 
+#' @format A \linkS4class{GuideSet} object.
+#'     
+#' @usage data(krasGuideSet, package="crisprViz")
+"krasGuideSet"
+
+
+
+#' Example GuideSet targeting the human GPR21 gene.
+#'
+#' An example \linkS4class{GuideSet} object targeting a section of the CDS of
+#'     the human GPR21 gene. Generated using the BE4max CRISPR nuclease, the
+#'     genome sequence from the \code{BSgenome.Hsapiens.UCSC.hg38} package, and
+#'     the gene model from \code{txdb_human} in the \code{crisprDesignData}
+#'     package (Ensembl release 104).
+#' 
+#' @format A \linkS4class{GuideSet} object.
+#'     
+#' @usage data(gpr21GuideSet, package="crisprViz")
+"gpr21GuideSet"
+
+
+
+#' Example GuideSet targeting the human MMP7 gene.
+#'
+#' An example \linkS4class{GuideSet} object targeting the promoter region of
+#'     the human MMP7 gene. Generated using the SpCas9 CRISPR nuclease, the
+#'     genome sequence from the \code{BSgenome.Hsapiens.UCSC.hg38} package, and
+#'     the gene model from \code{tss_human} in the \code{crisprDesignData}
+#'     package (Ensembl release 104).
+#' 
+#' @format A \linkS4class{GuideSet} object.
+#'     
+#' @usage data(mmp7GuideSet, package="crisprViz")
+"mmp7GuideSet"
+
+
+
+#' Example GuideSet targeting the human LTN1 gene.
+#'
+#' An example \linkS4class{GuideSet} object targeting the CDS of the human LTN1
+#'     gene. Generated using the SpCas9 CRISPR nuclease, the genome sequence
+#'     from the \code{BSgenome.Hsapiens.UCSC.hg38} package, and the gene model
+#'     from \code{txdb_human} in the \code{crisprDesignData} package (Ensembl
+#'     release 104).
+#' 
+#' @format A \linkS4class{GuideSet} object.
+#'     
+#' @usage data(cas9GuideSet, package="crisprViz")
+"cas9GuideSet"
+
+
+
+#' Example GuideSet targeting the human LTN1 gene.
+#'
+#' An example \linkS4class{GuideSet} object targeting the CDS of the human LTN1
+#'     gene. Generated using the AsCas12a CRISPR nuclease, the genome sequence
+#'     from the \code{BSgenome.Hsapiens.UCSC.hg38} package, and the gene model
+#'     from \code{txdb_human} in the \code{crisprDesignData} package (Ensembl
+#'     release 104).
+#' 
+#' @format A \linkS4class{GuideSet} object.
+#'     
+#' @usage data(cas12aGuideSet, package="crisprViz")
+"cas12aGuideSet"
+
+
+
+#' CAGE peak annotation from AnnotationHub
+#'
+#' A sample of CAGE peak annotation from AnnotationHub (ascension number
+#'     AH5084), lifted over to hg38.
+#' 
+#' @format A \linkS4class{GRanges} object.
+#'     
+#' @usage data(cage, package="crisprViz")
+"cage"
+
+
+
+#' DNase I hypersensitive site annotation from AnnotationHub
+#'
+#' A sample of DNase I hypersensitive site annotation from AnnotationHub
+#'     (ascension number AH30743), lifted over to hg38.
+#' 
+#' @format A \linkS4class{GRanges} object.
+#'     
+#' @usage data(dnase, package="crisprViz")
+"dnase"
